5943c55b562589dd386c6661ea3d40915ac95ddf
NGHI4 <110522520+NGHI4@users.noreply.github.com>
2023-03-11 11:00:06 +0700

    BaithiJava2

---
+++ src/baithijava2/AddressBook.java
@@ -0,0 +1,22 @@
+package baithijava2;
+public class AddressBook {
+
+    public static class Contact {
+        private String name;
+        private String phone;
+
+        public Contact(String name, String phone) {
+            this.name = name;
+            this.phone = phone;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public String getPhone() {
+            return phone;
+        }
+    }
+
+}
